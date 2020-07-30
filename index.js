d36e9a69295e242d9c51519eec2a25551b577f19
nicolaivasquez <nicolai.vasquez@cytora.com>
2020-07-30 18:28:28 +0100

    output zip file

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://data.police.uk/data/");
 
   await page.click("#id_forces_1");
   await page.click('button[type="submit"]');
+  await page.waitForRequest((request) => {
+    return request.url().includes("progress") && request.method() === "GET";
+  });
+  await page.waitForFunction(
+    'document.querySelector("#content > div > a").innerText.includes("Download now")'
+  );
 
-  // await browser.close();
+  const url = await page.evaluate(() => {
+    const linkWithUrl = document.evaluate(
+      '//*[@id="content"]/div[@role="main"]/a',
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    ).singleNodeValue;
+
+    return linkWithUrl.getAttribute("href");
+  });
+  console.log(url);
+
+  await browser.close();
 })();
